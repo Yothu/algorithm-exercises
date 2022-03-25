0d119a121d3e8c58f55458711ba2a20479fbcca8
David Vergaray <david.vergaray.a@gmail.com>
2022-03-25 11:12:26 -0300

    Update simple-array-sum.js

--- simple-array-sum.js
+++ simple-array-sum.js
@@ -1,7 +1,10 @@
-    
-    
-    let total = 0;
-    for(let i = 0;i < ar.length;i++){
-        total += ar[i];
-    }
-    return total;
+// Solution to the hackerrank problem in this link:
+// https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+
+function simpleArraySum(ar) {
+  let total = 0;
+  for(let i = 0;i < ar.length;i++){
+      total += ar[i];
+  }
+  return total;
+}
