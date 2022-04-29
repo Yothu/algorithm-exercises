b70a246ba0507179be34d89322e033aefa7a0db2
David Vergaray <david.vergaray.a@gmail.com>
2022-04-29 10:22:40 -0400

    Create you-are-a-square.js

---
+++ you-are-a-square.js
@@ -0,0 +1,6 @@
+// This is the answer to the coding problem found in this page: 
+// https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
+
+var isSquare = function(n){
+  return Number.isInteger(Math.sqrt(n)) ? true : false
+}
