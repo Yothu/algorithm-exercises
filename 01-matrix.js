9bd51897f832d1a6b33bf45b7d32c7b36f2464d3
David Vergaray <david.vergaray.a@gmail.com>
2022-07-29 17:45:26 -0400

    Update 01-matrix.js

--- 01-matrix.js
+++ 01-matrix.js
@@ -23,3 +23,42 @@ function updateMatrix(mat: number[][]): number[][] {
   
   return arr;
 };
+
+const blocksInDistance = (x: number, y: number, dis: number, arr: number[][]): boolean => {
+  let limit: number = (dis * 2) + 1;
+  let xSearch:number = x - dis;
+  let ySearch:number = 0;
+  const xMax = arr.length;
+  const yMax = arr[0].length;
+  
+  
+  if (xSearch < 0) {
+    ySearch = -xSearch;
+    limit -= ySearch;
+    xSearch = 0;
+  }
+  
+  while (limit > 0 && xSearch < xMax) {
+    let top = y + ySearch;
+    if (top >= 0) {
+      if (arr[xSearch][top] === 0) return false;
+    }
+    
+    let bottom = y - ySearch;
+    if (bottom < yMax) {
+      if (arr[xSearch][bottom] === 0) return false;
+    }
+    
+    xSearch += 1;
+    
+    if (xSearch <= x) {
+      ySearch += 1;
+    } else {
+      ySearch -= 1;
+    }
+    
+    
+    limit -= 1;
+  }
+  return true;
+};
